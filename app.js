3b37bd328760e2ff2355a3887cd5c416108a4395
Bonifase <orwabonifase@gmail.com>
2019-03-30 01:51:42 +0300

    set up eslint with nodemon

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Item = require('./models/itemModel');
 
 const app = express();
-
+const shoppingsRouter = express.Router();
+const db = mongoose.connect('mongodb://localhost/shoopingAPI');
 const port = process.env.PORT || 3000;
 
+
+shoppingsRouter.route('/shoppings')
+.get((req, res) => {
+
+  Item.find((error, shoppings) => {
+      if(error){
+          return res.send(error);
+      }
+      return res.json(shoppings);
+  });
+  });
+
+app.use('/api', shoppingsRouter);
+
 app.get('/', (req, res) => {
-  res.send('Hello my world api');
+  res.send('Hello my world api')
 });
 
 app.listen(port, () => {
